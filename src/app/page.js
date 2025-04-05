f82217b8135eb27e3d9552c2c1f4148ba927cede
Yarema Petrushchak <yarema.petrushchak24@gmail.com>
2025-04-04 20:38:32 -0400

    Update font imports and layout styles; change title and description in metadata

--- src/app/page.js
+++ src/app/page.js
@@ -5,10 +5,11 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="py-6 px-50  font-[family-name:var(--font-geist-sans)]">
+    <div className="px-8 py-6  lg:py-6 lg:px-50  font-[family-name:var(--font-noto-sans)]">
       <div className="h-dvh mb-10 flex flex-col gap-4">
         <Navbar />
         <HeroScroller />
+        <div className=" h-14"> </div>
       </div>
       <Footer />
     </div>
