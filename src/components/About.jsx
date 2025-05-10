1ec95248550da26ce08d2925eb335ef1bc3c4155
Yarema Petrushchak <yarema.petrushchak24@gmail.com>
2025-05-10 10:58:08 -0400

    Refactor About component layout and styling. Removed elemto ents Feature Componet2 to make and independent compunent to match new figma design.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,47 +14,33 @@ export default function About() {
         power of board games to bring people together, spark creativity, and create unforgettable moments. Our goal is
         to provide high-quality games for every type of player, whsther you’re a beginner or a seasoned strategist.
       </div>
-      {/* Icons */}
-      <div className="flex flex-row justify-around items-center w-full mb-10 text-center">
-        <div className="flex flex-col items-center justify-center gap-2 max-w-3xs">
-          <Image width={64} height={64} src={DICE_IMG} alt="Dice icon" />
-          <h3 className="text-lg lg:text-2xl">Wide Selection</h3>
-          <p className="text-sm lg:text-lg text-pretty max-w-3xl text-[#848484]">
-            From party games to deep strategy, we have something for everyone
-          </p>
-        </div>
-        <div className="flex flex-col items-center justify-center gap-2 max-w-3xs">
-          <Image width={64} height={64} src={MEDAL_IMG} alt="Medal icon" />
-          <h3 className="text-lg lg:text-2xl">High-Quality Games</h3>
-          <p className="text-sm lg:text-lg text-pretty max-w-3xl text-[#848484]">
-            We source directly from trusted manufacturers
-          </p>
-        </div>
-        <div className="flex flex-col items-center justify-center gap-2 max-w-3xs">
-          <Image width={64} height={64} src={QUESTION_IMG} alt="Question marc icon" />
-          <h3 className="text-lg lg:text-2xl">Support</h3>
-          <p className="text-sm lg:text-lg text-pretty max-w-3xl  text-[#848484]">
-            Our team is here to help you find the perfect game
-          </p>
-        </div>
-      </div>
 
       {/* Sign up for deals form */}
-      <div className="w-full py-22 bg-[#E6E6E6] flex flex-col items-center justify-center">
+      <div className="w-full py-22  flex flex-col items-center justify-center bg-[#494791] text-white">
         <h2 className="uppercase text-lg  lg:text-3xl mb-8 font-semibold">Stay Updated & Get Exclusive Deals!</h2>
 
         <p className="text-sm lg:text-lg text-pretty max-w-xl text-center mb-6">
           Subscribe to our newsletter and be the first to know about new arrivals, special offers, and gaming news
         </p>
-        <div className="flex flex-row gap-4 items-center justify-center w-full">
-          <input
-            type="email"
-            placeholder="Enter your email address"
-            className="w-full max-w-md p-2 border  border-black py-4 outline-gray-400"
-          />
-          <button className="bg-white text-[#494791] py-4 px-8  hover:bg-[#494791] hover:text-white transition-all duration-150">
-            Sign Up
-          </button>
+        <div className="flex flex-col w-full max-w-3xl gap-3 items-center">
+          <div className="flex flex-col md:flex-row w-full gap-4">
+            <input type="email" placeholder="Email" className="w-full md:flex-1 p-4 bg-white text-black outline-none" />
+            <button className="w-full md:w-auto bg-white text-[#494791] p-4 px-8 text-lg font-semibold uppercase hover:bg-gray-100 transition-all duration-150">
+              Subscribe
+            </button>
+          </div>
+
+          <div className="flex items-center gap-2 self-start max-w-4xl">
+            <input
+              type="checkbox"
+              id="marketing-consent"
+              name="marketing-consent"
+              className="h-5 w-5 border-2 accent-white cursor-pointer"
+            />
+            <label htmlFor="marketing-consent" className="text-sm md:text-base cursor-pointer">
+              I agree to receiving marketing emails and special deals
+            </label>
+          </div>
         </div>
       </div>
     </section>
