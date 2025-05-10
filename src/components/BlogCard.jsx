ec5b050ddabff24ff14752515b7d0ffb609ebf54
Yarema Petrushchak <yarema.petrushchak24@gmail.com>
2025-05-10 10:52:22 -0400

    Add blog images and update BlogCard component to use new image sources; updated BlogSection layout to match new Figma style

--- src/components/BlogCard.jsx
+++ src/components/BlogCard.jsx
@@ -1,23 +1,21 @@
 import Image from "next/image";
 import Link from "next/link";
 
-export default function BlogCard({ date, readingTime, name, category, imageUrl, blogUrl }) {
+export default function BlogCard({ date, readingTime, name, category, imageSrc, blogUrl }) {
   return (
     <article className=" flex flex-col ">
       {/* Blog Card Image */}
       <div className="relative h-48 w-full">
-        <Image src={imageUrl} alt={name} fill className="object-cover" />
+        <Image src={imageSrc} alt={name} fill className="object-cover" />
       </div>
       {/* Blog Card Details */}
-      <div className="flex flex-col justify-between gap-4">
+      <div className="flex flex-col justify-between mt-1">
         <p className="text-sm text-gray-500 mt-3">
           {date} - {readingTime} min read
         </p>
         <h3 className="text-lg font-medium ">{name}</h3>
-        <p className="text-white text-sm   ">
-          <span className="bg-[#9796BE] p-1 rounded-sm">{category}</span>
-        </p>
-        <Link href={blogUrl} className="text-sm  inline-block text-indigo-600 hover:underline">
+
+        <Link href={blogUrl} className="text-sm mt-3  inline-block text-indigo-600 hover:underline">
           Read full article<span className="inline-block ml-1">→</span>
         </Link>
       </div>
--- src/components/BlogSection.jsx
+++ src/components/BlogSection.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import BlogCard from "./BlogCard";
 
+import BLOG_IMG1 from "../../public/Blog/blog_1.png";
+import BLOG_IMG2 from "../../public/Blog/blog_2.png";
+import BLOG_IMG3 from "../../public/Blog/blog_3.png";
+
 export default function BlogSection() {
   const blogPosts = [
     {
@@ -9,7 +13,7 @@ export default function BlogSection() {
       readingTime: 5,
       name: "Catan -The Ultimate Board Game Experience?",
       category: "Game Review",
-      imageUrl: "700x700.svg",
+      imageSrc: BLOG_IMG1,
       blogUrl: "/blog/catan-ultimate-board-game-experience",
     },
     {
@@ -18,7 +22,7 @@ export default function BlogSection() {
       readingTime: 7,
       name: "D&D, How to Start Playing",
       category: "Game Tutorial",
-      imageUrl: "700x700.svg",
+      imageSrc: BLOG_IMG2,
       blogUrl: "/blog/future-of-react-2023",
     },
     {
@@ -27,14 +31,14 @@ export default function BlogSection() {
       readingTime: 40,
       name: "Warhammer 40k - The Ultimate Guide",
       category: "Game Review",
-      imageUrl: "700x700.svg",
+      imageSrc: BLOG_IMG3,
       blogUrl: "/blog/css-tips-and-tricks",
     },
   ];
 
   return (
-    <section className="  mb-25">
-      <div className="container mx-auto px-4">
+    <section className="  mb-25 px-8 lg:px-50">
+      <div className="container mx-auto ">
         <div className="flex items-center justify-between">
           <h2 className="text-4xl font-semibold tracking-wide uppercase mb-8">Check out our blogs</h2>
           <div className="self-start underline underline-offset-2">view all</div>
@@ -48,7 +52,7 @@ export default function BlogSection() {
               readingTime={post.readingTime}
               name={post.name}
               category={post.category}
-              imageUrl={post.imageUrl}
+              imageSrc={post.imageSrc}
               blogUrl={post.blogUrl}
             />
           ))}
