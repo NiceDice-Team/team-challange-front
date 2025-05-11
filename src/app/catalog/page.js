ee7b876a66ef637c9c04fb724cdfc9e7021e8ad7
Yarema Petrushchak <yarema.petrushchak24@gmail.com>
2025-05-10 22:13:21 -0400

    Update import statements to include file extensions for consistency

--- src/app/catalog/page.js
+++ src/app/catalog/page.js
@@ -1,9 +1,9 @@
 "use client";
-import Footer from "@/components/layout/Footer";
-import Navbar from "@/components/layout/Navbar";
-import ProductsGrid from "@/components/catalog/ProductsGrid";
+import Footer from "@/components/layout/Footer.jsx";
+import Navbar from "@/components/layout/Navbar.jsx";
+import ProductsGrid from "@/components/catalog/ProductsGrid.jsx";
 import { useState } from "react";
-import FilterSideBar from "@/components/catalog/FilterSideBar";
+import FilterSideBar from "@/components/catalog/FilterSideBar.jsx";
 
 export default function Catalog() {
   return (
