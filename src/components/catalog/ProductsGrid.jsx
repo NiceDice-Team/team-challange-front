06695a4ff8e6d285aea88318a3ec916197f5d1f5
Yarema Petrushchak <yarema.petrushchak24@gmail.com>
2025-05-17 11:18:22 -0400

    Enhance ProductsGrid component: implement product fetching with useQuery and display loading/error states

--- src/components/catalog/ProductsGrid.jsx
+++ src/components/catalog/ProductsGrid.jsx
@@ -1,11 +1,36 @@
 "use client";
 import { useQuery } from "@tanstack/react-query";
 import { useState } from "react";
+import { productServices } from "../../services/productServices";
 
 export default function ProductsGrid() {
+  const {
+    data: products,
+    isLoading: productsLoading,
+    error: productsError,
+  } = useQuery({
+    queryKey: ["products"],
+    queryFn: productServices.getProducts,
+  });
+
   return (
     <section className="">
-      <div className="grid grid-cols-4 grid-rows-4 gap-4"></div>
+      <div className="grid grid-cols-4 grid-rows-4 gap-4">
+        {productsLoading && <div>Loading products...</div>}
+        {productsError && <div>Error loading products</div>}
+        {products &&
+          products.map((product) => (
+            <div className="max-w-56" key={product.id}>
+              <h3>{product.title}</h3>
+              <p>{product.category}</p>
+              <img src={product.images[0].url} alt={product.title} />
+              <div>
+                <p>{product.description}</p>
+                <span>${product.price}</span>
+              </div>
+            </div>
+          ))}
+      </div>
     </section>
   );
 }
