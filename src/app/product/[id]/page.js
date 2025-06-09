975c55e257cbefdcbab2aed591cfc15b69a0c032
Yarema Petrushchak <yarema.petrushchak24@gmail.com>
2025-06-09 09:30:02 -0400

    Removed extra pages

--- src/app/product/[id]/page.js
+++
@@ -1,6 +0,0 @@
-import ProductPage from "../../../components/Product_page/Single_product_page";
-
-export default function ProductDetailPage({ params }) {
-  // You can use params.id to fetch the specific product data
-  return <ProductPage />;
-}
