3fc046d7cd65791e992ac7d1d290653523e3f164
Yarema Petrushchak <yarema.petrushchak24@gmail.com>
2025-06-09 09:31:58 -0400

    Added placehodler pages and layour page for auth routes.

---
+++ src/app/auth/layout.js
@@ -0,0 +1,16 @@
+import Navbar from "../../components/layout/Navbar.jsx";
+import Footer from "../../components/layout/Footer.jsx";
+
+export default function AuthLayout({ children }) {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <div className="px-8 lg:px-16 py-6">
+        <Navbar />
+      </div>
+
+      <main className="flex-1 px-8 lg:px-16 py-10">{children}</main>
+
+      <Footer />
+    </div>
+  );
+}
