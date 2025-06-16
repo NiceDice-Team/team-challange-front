0c73bc894f9c9a2f49be67463f4b2fe25640de22
MilaKoseckaya <kosetsaya@gmail.com>
2025-06-16 18:30:00 +0300

    feat: add cursor

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -73,7 +73,7 @@ export default function Navbar() {
           <LanguageSelector />
 
           {/* Profile Logo */}
-          <div>
+          <div className="cursor-pointer">
             <svg width="18" height="22" viewBox="0 0 18 22" fill="none">
               <path
                 d="M9 9C11.2091 9 13 7.20914 13 5C13 2.79086 11.2091 1 9 1C6.79086 1 5 2.79086 5 5C5 7.20914 6.79086 9 9 9Z"
