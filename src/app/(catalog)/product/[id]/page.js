c5f7c2588742a1280ca55ba8e3a4ae35a9602aae
Yarema Petrushchak <yarema.petrushchak24@gmail.com>
2025-06-18 00:12:03 -0400

    Added ReviewProduct, Navbar and Footer into ProductDetailPage.

--- src/app/(catalog)/product/[id]/page.js
+++ src/app/(catalog)/product/[id]/page.js
@@ -1,6 +1,16 @@
 import ProductPage from "../../../../components/catalog/ProductPage.jsx";
+import Navbar from "../../../../components/layout/Navbar.jsx";
+import Footer from "../../../../components/layout/Footer.jsx";
+import ReviewsProduct from "../../../../components/catalog/ReviewsProduct.jsx";
 
 export default function ProductDetailPage({ params }) {
   // You can use params.id to fetch the specific product data
-  return <ProductPage params={params} />;
+  return (
+    <>
+      <Navbar />
+      <ProductPage params={params} />
+      <ReviewsProduct />
+      <Footer />
+    </>
+  );
 }
