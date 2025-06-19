ba0c65e76ba5ab610179cdc36063bb415f5df44c
Yarema Petrushchak <yarema.petrushchak24@gmail.com>
2025-06-18 20:47:36 -0400

    Edited ReviewsProduct layout and styling

--- src/components/catalog/ReviewsProduct.jsx
+++ src/components/catalog/ReviewsProduct.jsx
@@ -4,7 +4,9 @@ export default function ReviewsProduct({ children }) {
   return (
     <section className="flex flex-col items-center justify-center  max-w-6xl mx-auto">
       {children}
-
+      <div className="mx-auto mb-10">
+        <h2 className="uppercase text-[40px]">Reviews from our customers</h2>
+      </div>
       <div className="grid grid-cols-3 gap-4 w-full h-28">
         {/* Total reviews */}
         <div className="flex flex-col justify-center items-center border-r-2 border-[var(--color-purple)]/50">
@@ -14,18 +16,30 @@ export default function ReviewsProduct({ children }) {
               4.90 out of 5
             </span>
           </div>
-          <span className="text-base font-medium">Based on 8 reviews</span>
+          <span className="text-base ">Based on 8 reviews</span>
         </div>
 
         {/* Star list */}
         <div className="flex flex-col justify-center items-center gap-1 ">
           {Array.from({ length: 5 }).map((_, index) => (
-            <StarRating rating={5} />
+            <div key={index} className="flex flex-nowrap justify-center items-center gap-2">
+              <StarRating rating={5} />
+              <progress
+                id="rating"
+                max="100"
+                value="30"
+                className="max-w-[88px] max-h-2 [&::-webkit-progress-bar]:bg-[#D9D9D9] [&::-webkit-progress-value]:bg-[#494791] [&::-moz-progress-bar]:bg-[#494791]"
+                style={{
+                  backgroundColor: "#D9D9D9",
+                }}
+              ></progress>
+              <span>7</span>
+            </div>
           ))}
         </div>
         {/* Write a review button */}
         <div className="flex flex-col justify-center items-center border-l-2 border-[var(--color-purple)]/50">
-          <button className="uppercase">Write a Review</button>
+          <button className="uppercase bg-[var(--color-purple)] text-white py-2 px-4 ">Write a Review</button>
         </div>
       </div>
       {/* Vertical divider */}
