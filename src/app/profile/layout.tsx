4d86acd816dcaece469fe2fa0a3d4cb390536e59
MilaKoseckaya <kosetsaya@gmail.com>
2025-08-20 13:33:56 +0300

    fix Navbar

--- src/app/profile/layout.tsx
+++ src/app/profile/layout.tsx
@@ -5,6 +5,7 @@ import logo from "../../../public/icons/Logo.svg";
 import Image from "next/image";
 import Link from "next/link";
 import { LogoutButton } from "@/components/auth/LogoutButton";
+import Navbar from "@/components/layout/Navbar";
 
 export default function ProfileLayout({
   children,
@@ -13,15 +14,8 @@ export default function ProfileLayout({
 }) {
   return (
     <div className="flex flex-col mx-auto min-h-screen container">
-      <div className="flex flex-row justify-between items-center shadow-header px-8 lg:px-16 py-6">
-        <Link href="/">
-          <Image src={logo} alt="logo" />
-        </Link>
-        <LogoutButton
-          className="bg-white hover:bg-gray-50 border border-purple w-[116px] text-purple hover:text-gray-900"
-          showText={true}
-          showIcon={true}
-        />
+      <div className="px-8 lg:px-16 py-6">
+        <Navbar isPagination={false} />
       </div>
       <main>{children}</main>
     </div>
