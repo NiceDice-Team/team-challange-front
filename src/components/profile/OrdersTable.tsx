3ede14f4d5873d8ae739f2c96e81f66530744eac
MilaKoseckaya <kosetsaya@gmail.com>
2025-08-29 00:09:41 +0300

    styled cells in table

--- src/components/profile/OrdersTable.tsx
+++ src/components/profile/OrdersTable.tsx
@@ -1,6 +1,7 @@
+import { Eye } from "lucide-react";
+import CustomBadge from "../shared/CustomBadge";
 import {
   Table,
-  TableCaption,
   TableHead,
   TableHeader,
   TableRow,
@@ -8,33 +9,54 @@ import {
   TableCell,
 } from "../ui/table";
 
+const formatItemsCount = (count: number): string => {
+  if (count === 1) {
+    return "1 item";
+  }
+  return `${count} items`;
+};
+
+const formatDate = (dateString: string): string => {
+  const date = new Date(dateString);
+  const day = date.getDate().toString().padStart(2, "0");
+  const month = (date.getMonth() + 1).toString().padStart(2, "0");
+  const year = date.getFullYear();
+  return `${day}.${month}.${year}`;
+};
+
 const orders = [
   {
-    id: 1,
+    id: 12345674,
     date: "2021-01-01",
     total: 112.0,
     status: "Delivered",
     items: 2,
   },
   {
-    id: 2,
+    id: 12345600,
     date: "2021-01-01",
     total: 56.0,
     status: "Shipped",
     items: 1,
   },
   {
-    id: 3,
+    id: 10045674,
     date: "2021-01-01",
     total: 230,
     status: "Processing",
     items: 3,
   },
 ];
+
+const mappingStatusesToVariant = {
+  Delivered: "default",
+  Shipped: "secondary",
+  Processing: "outline",
+};
+
 function OrdersTable() {
   return (
     <Table>
-      {/* <TableCaption>A list of your recent invoices.</TableCaption> */}
       <TableHeader className="mb-6 border-none">
         <TableRow className="text-dark-gray uppercase">
           <TableHead className="w-[155px]">ORDER ID</TableHead>
@@ -49,11 +71,17 @@ function OrdersTable() {
         {orders.map((order) => (
           <TableRow key={order.id} className="py-4">
             <TableCell className="py-4 font-medium">{order.id}</TableCell>
-            <TableCell>{order.date}</TableCell>
-            <TableCell>{order.status}</TableCell>
-            <TableCell>{order.items}</TableCell>
-            <TableCell>{order.total}</TableCell>
-            <TableCell className="text-right">View</TableCell>
+            <TableCell>{formatDate(order.date)}</TableCell>
+            <TableCell>
+              <CustomBadge variant={mappingStatusesToVariant[order.status]}>
+                {order.status}
+              </CustomBadge>
+            </TableCell>
+            <TableCell>{formatItemsCount(order.items)}</TableCell>
+            <TableCell>${order.total.toFixed(2)}</TableCell>
+            <TableCell className="flex justify-end items-center gap-2 h-[53px] text-right">
+              <Eye size={20} /> View
+            </TableCell>
           </TableRow>
         ))}
       </TableBody>
