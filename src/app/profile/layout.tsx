d1a104f8cf35d81cd73411bbd1928dc84dec4454
MilaKoseckaya <kosetsaya@gmail.com>
2025-11-21 00:27:27 +0200

    fix profile layout

--- src/app/profile/layout.tsx
+++ src/app/profile/layout.tsx
@@ -13,13 +13,14 @@ export default function ProfileLayout({
     <>
       <div className="flex flex-col mx-auto min-h-screen container">
         <div className="px-8 lg:px-16 py-6">
-          <Navbar isPagination={false} />
+          {/* <Navbar isPagination={false} /> */}
         </div>
-        
-        <main>{children}</main>
-        <Footer />
+
+        <main className="flex-1 px-4 md:px-8 lg:px-0 py-4 md:py-0">
+          {children}
+        </main>
+        {/* <Footer /> */}
       </div>
-      
     </>
   );
 }
