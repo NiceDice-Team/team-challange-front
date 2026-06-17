115e5fa1f3b7cb6f5c94ba98a563a352ea6ae74f
milakoseckaya <kosetsaya@gmail.com>
2026-06-17 23:50:29 +0300

    fix checkout responsive

--- src/components/checkout/DeliveryOptions.tsx
+++ src/components/checkout/DeliveryOptions.tsx
@@ -35,15 +35,24 @@ const DeliveryOptions = ({
     if (!deliveryOptions?.length) return;
 
     const first = deliveryOptions[0];
-    const isPaymentValid = payment && deliveryOptions.some((option) => option.id === payment.id);
+    const isPaymentValid =
+      payment && deliveryOptions.some((option) => option.id === payment.id);
 
     if (!payment || !isPaymentValid) {
       setPaymentMethod(first);
       onPaymentMethodChange(first);
     }
-  }, [deliveryOptions, deliveryOptionsLoading, onPaymentMethodChange, payment, setPaymentMethod]);
+  }, [
+    deliveryOptions,
+    deliveryOptionsLoading,
+    onPaymentMethodChange,
+    payment,
+    setPaymentMethod,
+  ]);
 
-  const handlePaymentMethodChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handlePaymentMethodChange = (
+    e: React.ChangeEvent<HTMLInputElement>,
+  ) => {
     const selectedId = Number(e.target.value);
     const method = deliveryOptions.find((option) => option.id === selectedId);
 
@@ -64,7 +73,7 @@ const DeliveryOptions = ({
         <div className="text-gray-2">No delivery options available</div>
       ) : (
         <>
-          <div className="flex flex-col gap-3">
+          <div className="flex flex-col gap-14 lg:gap-3">
             {deliveryOptions.map((option) => (
               <RadioButton
                 className="flex items-center gap-3 h-10"
@@ -75,7 +84,7 @@ const DeliveryOptions = ({
                 checked={selectedDeliveryOption?.id === option.id}
                 onChange={handlePaymentMethodChange}
               >
-                <div className="flex gap-2">
+                <div className="flex gap-0 flex-col lg:flex-row lg:gap-2 ">
                   <p className="font-bold text-purple">${option.price}</p>
                   <h3 className="font-medium uppercase">{option.name}</h3>
                   <p className="text-gray-2">{option.description}</p>
@@ -85,17 +94,20 @@ const DeliveryOptions = ({
           </div>
           <div className="flex items-center gap-2 mb-10">
             <Image src={icon} alt="info" className="w-6 h-6" />
-            <p className="max-w-[566px] text-gray-2">
-              Please note that all international shipments may be subject to customs duties and taxes depending on your
-              local regulations. These charges are the full responsibility of the buyer and are not included in the item
-              price.
+            <p className="max-w-[566px] text-gray-2 mt-5 lg:mt-0">
+              Please note that all international shipments may be subject to
+              customs duties and taxes depending on your local regulations.
+              These charges are the full responsibility of the buyer and are not
+              included in the item price.
             </p>
           </div>
         </>
       )}
       <div className="flex justify-between items-center mb-2 font-bold uppercase px-6">
         <span>Shipping</span>
-        <span>{selectedDeliveryOption ? `$${selectedDeliveryOption.price}` : "-"}</span>
+        <span>
+          {selectedDeliveryOption ? `$${selectedDeliveryOption.price}` : "-"}
+        </span>
       </div>
       <div className="border-purple/50 border-t w-full h-px"></div>
     </div>
